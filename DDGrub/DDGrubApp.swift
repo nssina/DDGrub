baf3c34e2f6fb2e33e0560c8171d0fc5315b9e7f
Sina Rabiei <rabie_sina@yahoo.com>
2021-09-06 07:58:49 +0200

    Initial commit

---
+++ DDGrub/DDGrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DDGrubApp.swift
+//  DDGrub
+//
+//  Created by Sina Rabiei on 9/6/21.
+//
+
+import SwiftUI
+
+@main
+struct DDGrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
