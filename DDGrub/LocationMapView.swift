944bb0f2b773066d2e4809d3edb26949e2d0c098
Sina Rabiei <rabie_sina@yahoo.com>
2021-09-08 17:22:58 +0200

    Added map with coordinateRegion

--- DDGrub/LocationMapView.swift
+++ DDGrub/LocationMapView.swift
@@ -6,10 +6,18 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct LocationMapView: View {
+    
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37.331516,
+                                                                                  longitude: -121.891054),
+                                                   span: MKCoordinateSpan(latitudeDelta: 0.01,
+                                                                          longitudeDelta: 0.01))
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Map(coordinateRegion: $region)
+        }
     }
 }
 
