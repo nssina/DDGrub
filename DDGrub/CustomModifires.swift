f79a8064f756fe2791590ad1c596140b24513b2f
Sina Rabiei <rabie_sina@yahoo.com>
2021-10-14 18:12:33 +0200

    Created CustomModifires file

---
+++ DDGrub/CustomModifires.swift
@@ -0,0 +1,17 @@
+//
+//  CustomModifires.swift
+//  DDGrub
+//
+//  Created by Sina Rabiei on 10/14/21.
+//
+
+import SwiftUI
+
+struct ProfileNameText: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.system(size: 32, weight: .bold))
+            .lineLimit(1)
+            .minimumScaleFactor(0.75)
+    }
+}
