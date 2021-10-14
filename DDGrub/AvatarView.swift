5b21bbb324719a2c6cf6ddf044594166c8a74f36
Sina Rabiei <rabie_sina@yahoo.com>
2021-10-14 17:47:38 +0200

    Refactored AvatarView

---
+++ DDGrub/AvatarView.swift
@@ -0,0 +1,27 @@
+//
+//  AvatarView.swift
+//  DDGrub
+//
+//  Created by Sina Rabiei on 10/14/21.
+//
+
+import SwiftUI
+
+struct AvatarView: View {
+    
+    var size: CGFloat
+    
+    var body: some View {
+        Image("default-avatar")
+            .resizable()
+            .scaledToFit()
+            .frame(width: size, height: size)
+            .clipShape(Circle())
+    }
+}
+
+struct AvatarView_Previews: PreviewProvider {
+    static var previews: some View {
+        AvatarView(size: 90)
+    }
+}
