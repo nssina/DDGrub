50307c895de8ece7ea200da4945f7b5488d743e0
Sina Rabiei <rabie_sina@yahoo.com>
2021-11-05 20:00:04 +0100

    Added LocationMapViewModel

---
+++ DDGrub/Views/Screens/LocationMapView/LocationMapView.swift
@@ -0,0 +1,46 @@
+//
+//  LocationMapView.swift
+//  DDGrub
+//
+//  Created by Sina Rabiei on 9/6/21.
+//
+
+import SwiftUI
+import MapKit
+
+struct LocationMapView: View {
+    
+    @StateObject private var viewModel = LocationMapViewModel()
+    
+    var body: some View {
+        ZStack {
+            Map(coordinateRegion: $viewModel.region).ignoresSafeArea()
+            
+            VStack {
+                LogoView().shadow(radius: 10)
+                Spacer()
+            }
+        }
+        .alert(item: $viewModel.alertItem, content: { alertItem in
+            Alert(title: alertItem.title, message: alertItem.message, dismissButton: alertItem.dismissButton)
+        })
+        .onAppear {
+            viewModel.getLocations()
+        }
+    }
+}
+
+struct LocationMapView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationMapView()
+    }
+}
+
+struct LogoView: View {
+    var body: some View {
+        Image("ddg-map-logo")
+            .resizable()
+            .scaledToFit()
+            .frame(height: 70)
+    }
+}
