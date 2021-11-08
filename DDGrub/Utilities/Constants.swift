b8ce9d196f83d6653f3c60882a038425c64e74af
Sina Rabiei <rabie_sina@yahoo.com>
2021-11-08 19:09:28 +0100

    Added new enums in Constants file

--- DDGrub/Utilities/Constants.swift
+++ DDGrub/Utilities/Constants.swift
@@ -5,9 +5,29 @@
 //  Created by Sina Rabiei on 11/5/21.
 //
 
-import Foundation
+import UIKit
 
 enum RecordType {
     static let location = "DDGLocation"
     static let profile  = "DDGProfile"
+}
+
+
+enum PlaceholderImage {
+    static let avatar = UIImage(named: "default-avatar")!
+    static let square = UIImage(named: "default-square-asset")!
+    static let banner = UIImage(named: "default-banner-asset")!
+}
+
+enum ImageDimension {
+    case square, banner
+    
+    static func getPlaceholder(for dimension: ImageDimension) -> UIImage {
+        switch dimension {
+        case .square:
+            return PlaceholderImage.square
+        case .banner:
+            return PlaceholderImage.banner
+        }
+    }
 }
