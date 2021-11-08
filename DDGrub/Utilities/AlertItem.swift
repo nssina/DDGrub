67a43ca718c038d6ad413ca3466c2fa43289cca1
Sina Rabiei <rabie_sina@yahoo.com>
2021-11-08 19:09:12 +0100

    Added new alert for handling location permissions

--- DDGrub/Utilities/AlertItem.swift
+++ DDGrub/Utilities/AlertItem.swift
@@ -20,4 +20,13 @@ struct AlertContext {
     static let unableToGetLocations = AlertItem(title: Text("Location Error"),
                                             message: Text("Unable to retrive location at this time.\nPlease try again."),
                                                 dismissButton: .default(Text("OK")))
+    static let locationRestricted = AlertItem(title: Text("Location Restricted"),
+                                            message: Text("Your location is restricted. This may be due to parental controls."),
+                                                dismissButton: .default(Text("OK")))
+    static let locationDeined = AlertItem(title: Text("Location Deined"),
+                                            message: Text("DDGrub does not have permission to access your location. To change that go to your phone's Settings > DDGrub > Location."),
+                                                dismissButton: .default(Text("OK")))
+    static let locationDisabled = AlertItem(title: Text("Location Disabled"),
+                                            message: Text("Your phone's location services are disabled. To change that go to your phone's Settings > Privacy > Location Services."),
+                                                dismissButton: .default(Text("OK")))
 }
