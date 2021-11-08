8af05a2ee8b00cd8a55000653280ff47389052a5
Sina Rabiei <rabie_sina@yahoo.com>
2021-11-08 17:15:30 +0100

    Added LocationManager

---
+++ DDGrub/Utilities/Managers/LocationManager.swift
@@ -0,0 +1,12 @@
+//
+//  LocationManager.swift
+//  DDGrub
+//
+//  Created by Sina Rabiei on 11/8/21.
+//
+
+import Foundation
+
+final class LocationManager: ObservableObject {
+    @Published var locations: [DDGLocation] = []
+}
