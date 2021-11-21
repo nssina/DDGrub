25d031c182c301518aa47eea5f0c6b24c4aaba01
Sina Rabiei <rabie_sina@yahoo.com>
2021-11-21 14:38:12 +0100

    Added UIImage extension
    • Converting UIImage into the CKAsset

---
+++ DDGrub/Utilities/Extensions/UIImage+Ext.swift
@@ -0,0 +1,27 @@
+//
+//  UIImage+Ext.swift
+//  DDGrub
+//
+//  Created by Sina Rabiei on 11/21/21.
+//
+
+import CloudKit
+import UIKit
+
+extension UIImage {
+    func convertToCKAsset() -> CKAsset? {
+        
+        guard let urlPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { return nil }
+        
+        let fileURL = urlPath.appendingPathComponent("selectedAvatarImage")
+        
+        guard let imageData = jpegData(compressionQuality: 0.25) else { return nil }
+        
+        do {
+            try imageData.write(to: fileURL)
+            return CKAsset(fileURL: fileURL)
+        } catch {
+            return nil
+        }
+    }
+}
