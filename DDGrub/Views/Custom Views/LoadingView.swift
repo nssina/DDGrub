0e5fd444f36790ca97a181c2c604504a5e4813bd
Sina Rabiei <rabie_sina@yahoo.com>
2021-11-28 08:21:56 +0100

    Added LoadingView

---
+++ DDGrub/Views/Custom Views/LoadingView.swift
@@ -0,0 +1,29 @@
+//
+//  LoadingView.swift
+//  DDGrub
+//
+//  Created by Sina Rabiei on 11/28/21.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        ZStack {
+            Color(.systemBackground)
+                .opacity(0.9)
+                .ignoresSafeArea()
+            
+            ProgressView()
+                .progressViewStyle(CircularProgressViewStyle(tint: .brandPrimary))
+                .scaleEffect(2)
+                .offset(y: -40)
+        }
+    }
+}
+
+struct LoadingView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadingView()
+    }
+}
