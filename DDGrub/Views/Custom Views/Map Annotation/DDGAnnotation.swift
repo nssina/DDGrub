a8660fc955e79bc0199d74a7dace9eb2905e5ed0
nssina <rabie_sina@yahoo.com>
2022-05-25 21:23:59 +0200

    Created new DDGAnnotation

---
+++ DDGrub/Views/Custom Views/Map Annotation/DDGAnnotation.swift
@@ -0,0 +1,20 @@
+//
+//  DDGAnnotation.swift
+//  DDGrub
+//
+//  Created by Sina Rabiei on 5/25/22.
+//
+
+import SwiftUI
+
+struct DDGAnnotation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DDGAnnotation_Previews: PreviewProvider {
+    static var previews: some View {
+        DDGAnnotation()
+    }
+}
