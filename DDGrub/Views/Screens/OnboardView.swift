3a128c2bc0eaa3faf506100024e79caa4534a622
Sina Rabiei <rabie_sina@yahoo.com>
2022-06-05 21:07:04 +0200

    Refactored OnBoardingView

--- DDGrub/Views/Screens/OnboardView.swift
+++ DDGrub/Views/Screens/OnboardView.swift
@@ -8,27 +8,37 @@
 import SwiftUI
 
 struct OnboardView: View {
-    @Binding var isShowingOnboardView: Bool
+    
+    @Environment(\.presentationMode) var presentationMode
     
     var body: some View {
         VStack {
             HStack {
                 Spacer()
                 Button {
-                    isShowingOnboardView = false
+                    presentationMode.wrappedValue.dismiss()
                 } label: {
-                    XDismissButton()
+                   XDismissButton()
                 }
                 .padding()
             }
+            
             Spacer()
-            LogoView(frameWidth: 250)
-                .padding(.bottom)
+            
+            LogoView(frameWidth: 250).padding(.bottom)
             
             VStack(alignment: .leading, spacing: 32) {
-                OnboardInfoView(imageName: "building.2.crop.circle", title: "Restaurant Locations", description: "Find places to dine around the convention center.")
-                OnboardInfoView(imageName: "checkmark.circle", title: "Check In", description: "Let other iOS Developers know where you are.")
-                OnboardInfoView(imageName: "person.2.circle", title: "Find Friends", description: "See where other iOS Developers are and enjoy the party.")
+                OnboardInfoView(imageName: "building.2.crop.circle",
+                                title: "Restaurant Locations",
+                                description: "Find places to dine around the convention center")
+                
+                OnboardInfoView(imageName: "checkmark.circle",
+                                title: "Check In",
+                                description: "Let other iOS Devs know where you are")
+                
+                OnboardInfoView(imageName: "person.2.circle",
+                                title: "Find Friends",
+                                description: "See where other iOS Devs are and join the party")
             }
             .padding(.horizontal, 40)
             
@@ -39,11 +49,11 @@ struct OnboardView: View {
 
 struct OnboardView_Previews: PreviewProvider {
     static var previews: some View {
-        OnboardView(isShowingOnboardView: .constant(true))
+        OnboardView()
     }
 }
 
-struct OnboardInfoView: View {
+fileprivate struct OnboardInfoView: View {
     
     var imageName: String
     var title: String
@@ -51,7 +61,7 @@ struct OnboardInfoView: View {
     
     var body: some View {
         HStack(spacing: 26) {
-            Image(systemName: imageName)
+            Image(systemName: imageName )
                 .resizable()
                 .frame(width: 50, height: 50)
                 .foregroundColor(.brandPrimary)
