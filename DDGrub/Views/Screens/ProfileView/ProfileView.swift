d212852aa2fef3a409c62487b32965f5ad532d1f
nssina <rabie_sina@yahoo.com>
2022-06-05 18:56:07 +0200

    Added accessibility support (voice over) for the profileView

--- DDGrub/Views/Screens/ProfileView/ProfileView.swift
+++ DDGrub/Views/Screens/ProfileView/ProfileView.swift
@@ -23,6 +23,10 @@ struct ProfileView: View {
                             AvatarView(image: viewModel.avatar, size: 84)
                             EditImage()
                         }
+                        .accessibilityElement(children: .ignore)
+                        .accessibilityAddTraits(.isButton)
+                        .accessibilityLabel(Text("Profile Photo"))
+                        .accessibilityHint(Text("Opens the iPhone's photo picker"))
                         .padding(.leading, 12)
                         .onTapGesture { viewModel.isShowingPhotoPicker = true }
                         
@@ -40,6 +44,7 @@ struct ProfileView: View {
                 VStack(alignment: .leading, spacing: 8) {
                     HStack {
                         CharactersRemainView(currentCount: viewModel.bio.count)
+                            .accessibilityAddTraits(.isHeader)
                         Spacer()
                         
                         if viewModel.isCheckedIn {
@@ -55,6 +60,7 @@ struct ProfileView: View {
                                     .background(Color.grubRed)
                                     .cornerRadius(8)
                             }
+                            .accessibilityLabel(Text("Check out of current location"))
                         }
                     }
                     
@@ -62,6 +68,8 @@ struct ProfileView: View {
                         .frame(height: 100)
                         .overlay(RoundedRectangle(cornerRadius: 8)
                                     .stroke(Color.secondary, lineWidth: 1))
+                        .accessibilityLabel(Text("Bio, \(viewModel.bio)"))
+                        .accessibilityHint(Text("This TextField has a 100 character maximum."))
                 }
                 .padding(.horizontal, 20)
                 
