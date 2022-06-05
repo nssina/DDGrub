f8ba39c279a59486e0b7bbbbada7a2290ddc558a
Sina Rabiei <rabie_sina@yahoo.com>
2022-06-05 20:48:29 +0200

    Added new embedInScrollView func

--- DDGrub/Utilities/Extensions/View+Ext.swift
+++ DDGrub/Utilities/Extensions/View+Ext.swift
@@ -8,14 +8,26 @@
 import SwiftUI
 
 extension View {
+    
     func profileNameStyle() -> some View {
         self.modifier(ProfileNameText())
     }
+    
     
     func playHaptic() {
         let generator = UINotificationFeedbackGenerator()
         generator.notificationOccurred(.success)
     }
+    
+    
+    func embedInScrollView() -> some View {
+        GeometryReader { geometry in
+            ScrollView {
+                self.frame(minHeight: geometry.size.height, maxHeight: .infinity)
+            }
+        }
+    }
+    
     
     func dismissKeyboard() {
         UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
