fb6084e1feeba4b0f9cd619984b261cbeeb36310
Sina Rabiei <rabie_sina@yahoo.com>
2022-06-05 20:57:20 +0200

    Code shining

--- DDGrub/Views/Screens/LocationListView/LocationListView.swift
+++ DDGrub/Views/Screens/LocationListView/LocationListView.swift
@@ -7,19 +7,22 @@
 
 import SwiftUI
 
+import SwiftUI
+
 struct LocationListView: View {
     
     @EnvironmentObject private var locationManager: LocationManager
     @StateObject private var viewModel = LocationListViewModel()
+    @Environment(\.sizeCategory) var sizeCategory
     
     var body: some View {
         NavigationView {
             List {
                 ForEach(locationManager.locations) { location in
-                    NavigationLink(destination: LocationDetailView(viewModel: LocationDetailViewModel(location: location))) {
+                    NavigationLink(destination: viewModel.createLocationDetailView(for: location, in: sizeCategory)) {
                         LocationCell(location: location, profiles: viewModel.checkedInProfiles[location.id, default: []])
-                        .accessibilityElement(children: .ignore)
-                        .accessibilityLabel(Text(viewModel.createVoiceOverSummary(for: location)))
+                            .accessibilityElement(children: .ignore)
+                            .accessibilityLabel(Text(viewModel.createVoiceOverSummary(for: location)))
                     }
                 }
             }
