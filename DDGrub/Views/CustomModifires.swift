88f49909e0829aa38a25c6f7d76d153f0965bc72
Sina Rabiei <sina.rabiei@sectoralarm.com>
2022-06-26 20:03:50 +0200

    Update CustomModifires

--- DDGrub/Views/CustomModifires.swift
+++ DDGrub/Views/CustomModifires.swift
@@ -8,10 +8,12 @@
 import SwiftUI
 
 struct ProfileNameText: ViewModifier {
+    
     func body(content: Content) -> some View {
         content
             .font(.system(size: 32, weight: .bold))
             .lineLimit(1)
             .minimumScaleFactor(0.75)
+            .disableAutocorrection(true)
     }
 }
