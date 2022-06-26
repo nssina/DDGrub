70baaa8f3864eb6cb7716876707e00a2860b01d4
Sina Rabiei <sina.rabiei@sectoralarm.com>
2022-06-26 20:17:53 +0200

    Update UIKit components

--- DDGrub/Views/Custom Views/UIKit Components/PhotoPicker.swift
+++ DDGrub/Views/Custom Views/UIKit Components/PhotoPicker.swift
@@ -10,7 +10,6 @@ import SwiftUI
 struct PhotoPicker: UIViewControllerRepresentable {
     
     @Binding var image: UIImage
-    @Environment(\.presentationMode) var presentationMode
     
     func makeUIViewController(context: Context) -> UIImagePickerController {
         let picker = UIImagePickerController()
@@ -21,24 +20,17 @@ struct PhotoPicker: UIViewControllerRepresentable {
     
     func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
     
-    func makeCoordinator() -> Coordinator {
-        Coordinator(photoPicker: self)
-    }
+    func makeCoordinator() -> Coordinator { Coordinator(photoPicker: self) }
     
     final class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
         
         let photoPicker: PhotoPicker
         
-        init(photoPicker: PhotoPicker) {
-            self.photoPicker = photoPicker
-        }
+        init(photoPicker: PhotoPicker) { self.photoPicker = photoPicker }
         
         func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-            if let image = info[.editedImage] as? UIImage {
-                photoPicker.image = image
-            }
-            
-            photoPicker.presentationMode.wrappedValue.dismiss()
+            if let image = info[.editedImage] as? UIImage { photoPicker.image = image }
+            picker.dismiss(animated: true)
         }
     }
 }
